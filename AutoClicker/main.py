2bc341c197f8d815e0b57b2ae8a8fbb72ea2076d
Gavin Anderson <ganderson96@live.com>
2020-05-10 17:39:58 -0400

    Added random clicking inbetween bounds instead of a set click Added 'e' keybound press Added printing to show what the system is doing Added 10% chance of pressing 'e' so nothing is routine

--- AutoClicker/main.py
+++ AutoClicker/main.py
@@ -1,13 +1,19 @@
 import time
 import threading
-from pynput.mouse import Button, Controller
-from pynput.keyboard import Listener, KeyCode
+from random import random
+import numpy as np
+from pynput.mouse import Button as pyB, Controller
+from pynput.keyboard import Listener, KeyCode, Key, Controller as C
+from tkinter import *
 
-
-delay = 0.01
-button = Button.left
-start_stop_key = KeyCode(char='s')
-exit_key = KeyCode(char='e')
+#one swing
+minDelay = 2.4
+#4 swings and .1 seconds
+maxDelay = 9.7
+everyMinute = 60
+buttonLeftClick = pyB.left
+start_stop_key = KeyCode(char='=')
+exit_key = KeyCode(char='+')
 
 
 class ClickMouse(threading.Thread):
@@ -19,9 +25,11 @@ class ClickMouse(threading.Thread):
         self.program_running = True
 
     def start_clicking(self):
+        print("Started Clicking")
         self.running = True
 
     def stop_clicking(self):
+        print("Stopped Clicking")
         self.running = False
 
     def exit(self):
@@ -31,15 +39,48 @@ class ClickMouse(threading.Thread):
     def run(self):
         while self.program_running:
             while self.running:
+                delay = np.random.randint(minDelay,maxDelay)
+                #10% chance of storing ore
+                randPercent = np.random.randint(1,101)
+                if  randPercent>90:
+                    print('e clicked')
+                    keyboard.press('e')
+                    keyboard.release('e')
+
                 mouse.click(self.button)
-                time.sleep(self.delay)
+                time.sleep(delay)
 
+def click():
+    cm = ClickMouse()
+    if self.running == True:
+        cm.stop_clicking()
+    else:
+        cm.start_clicking()
 
+keyboard = C()
 mouse = Controller()
-click_thread = ClickMouse(delay, button)
+click_thread = ClickMouse(1, buttonLeftClick)
 click_thread.start()
+"""
+#Create new window object
+window = Tk()
+window.title ("Random auto clicker")
+#Define 2 lables min and max delay
+l1= Label(window, text = "Min Delay:").grid(row=0, column = 0)
+l2= Label(window, text = "Max Delay:").grid(row=0, column = 2)
+
+txtMinDelay = StringVar()
+e1 = Entry(window,textvariable =txtMinDelay).grid(row=0,column = 1)
+txtMaxDelay = StringVar()
+e2 = Entry(window,textvariable =txtMaxDelay).grid(row=0,column = 3)
+
+#define buttons
+btnStartStop=Button(window,text = "Start/Stop", width = 12, command = click).grid(row=2,column=3)
+btnQuit=Button(window,text = "Quit", width = 12, command = exit).grid(row=2,column=4)
 
 
+window.mainloop()
+"""
 def on_press(key):
     if key == start_stop_key:
         if click_thread.running:
